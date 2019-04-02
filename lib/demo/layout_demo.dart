059df8e422da4d80da4e3082b399e7e4599b35bb
togo.huang <huangshengtao365@zzumall.com>
2019-04-02 20:36:58 +0800

    Row 和 Column 布局

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,10 +3,22 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    /*return Column(
       children: <Widget>[
         IconBadge(Icons.pool,size: 64.0,)
       ],
+    );*/
+    return Container(
+      child: Column(  //Row横着排,Column竖着排
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+//        主轴, Row的主轴是横向的,Column的主轴是纵向的
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, //spaceEvenly表示平均分
+        children: <Widget>[
+          IconBadge(Icons.pool),
+          IconBadge(Icons.beach_access,size:64.0),
+          IconBadge(Icons.airplanemode_active)
+        ],
+      ),
     );
   }
 }

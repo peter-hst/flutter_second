c3d6a46393c4c2192dfa144fa181646abb1bc05c
togo.huang <huangshengtao365@zzumall.com>
2019-04-02 21:05:05 +0800

    宽高比容器设置

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -18,62 +18,10 @@ class LayoutDemo extends StatelessWidget {
 //          IconBadge(Icons.pool),
 //          IconBadge(Icons.beach_access,size:64.0),
 //          IconBadge(Icons.airplanemode_active)
-          Stack(
-            alignment: Alignment.topRight, //顶部靠右,这个对齐指的是小部件的对齐,大部件不受影响
-            children: <Widget>[
-              SizedBox(
-                  child: Container(
-                width: 200.0,
-                height: 300.0,
-                alignment: Alignment(0.0, -0.8),
-                // 对齐
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(3, 54, 255, 1.0),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-
-              )),
-              SizedBox(height: 32.0),
-              SizedBox(
-                  child: Container(
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(3, 54, 255, 1.0),
-//                borderRadius: BorderRadius.circular(8.0),
-                    shape: BoxShape.circle,
-                    gradient: RadialGradient(
-                        colors: [Color.fromRGBO(7, 102, 255, 1.0),Color.fromRGBO(3, 54, 255, 1.0),])),
-                child:
-                    Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
-              )),
-              Positioned(
-                left: 20.0,
-                top:120.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 24.0),
-              ),
-              Positioned(
-                left: 70.0,
-                top:180.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 20.0),
-              ),
-              Positioned(
-                left: 30.0,
-                top:230.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 18.0),
-              ),
-              Positioned(
-                left: 90.0,
-                top:20.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 12.0),
-              ),
-              Positioned(
-                left: 4.0,
-                top:-4.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
-              ),
-            ],
-          )
+//          StackDemo()
+          AspectRatio(
+              aspectRatio: 16.0 / 9.0, //设置宽高比例这样就得到 16:9 长:宽的容器
+              child: Container(color: Color.fromRGBO(3, 54, 255, 1.0)))
         ],
       ),
     );
@@ -93,6 +41,68 @@ class IconBadge extends StatelessWidget {
       width: size + 60,
       height: size + 60,
       color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight, //顶部靠右,这个对齐指的是小部件的对齐,大部件不受影响
+      children: <Widget>[
+        SizedBox(
+            child: Container(
+          width: 200.0,
+          height: 300.0,
+          alignment: Alignment(0.0, -0.8),
+          // 对齐
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(3, 54, 255, 1.0),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        )),
+        SizedBox(height: 32.0),
+        SizedBox(
+            child: Container(
+          width: 100.0,
+          height: 100.0,
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+//                borderRadius: BorderRadius.circular(8.0),
+              shape: BoxShape.circle,
+              gradient: RadialGradient(colors: [
+                Color.fromRGBO(7, 102, 255, 1.0),
+                Color.fromRGBO(3, 54, 255, 1.0),
+              ])),
+          child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
+        )),
+        Positioned(
+          left: 20.0,
+          top: 120.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 24.0),
+        ),
+        Positioned(
+          left: 70.0,
+          top: 180.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 20.0),
+        ),
+        Positioned(
+          left: 30.0,
+          top: 230.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 18.0),
+        ),
+        Positioned(
+          left: 90.0,
+          top: 20.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 12.0),
+        ),
+        Positioned(
+          left: 4.0,
+          top: -4.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+        ),
+      ],
     );
   }
 }

f825d897e962e2b759bfa42d164f328cc2ad599d
togo.huang <huangshengtao365@zzumall.com>
2019-04-02 15:57:17 +0800

    完成TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,53 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 //      debugShowCheckedModeBanner: false, //去掉debug标签
-      home: Home(),
-      theme: ThemeData(primaryColor: Colors.yellow) // app顶部bar颜色
+        home: Home(),
+        theme: ThemeData(primaryColor: Colors.yellow) // app顶部bar颜色
     );
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text('B标题'),
-          elevation: 0.0, //appbar 阴影 默认4.0
-        ),
-        body: ListViewDemo()
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              leading: IconButton( //appbar 左边的按钮
+                  icon: Icon(Icons.menu),
+                  tooltip: 'Navigration',
+                  onPressed: () =>
+                      debugPrint('Navigration button is pressed.')),
+              title: Text('B标题'),
+              actions: <Widget>[
+                IconButton( //appbar
+                    icon: Icon(Icons.search),
+                    tooltip: 'Search',
+                    onPressed: () => debugPrint('Search button is pressed.')),
+                IconButton( //appbar
+                    icon: Icon(Icons.more_horiz),
+                    tooltip: 'More',
+                    onPressed: () => debugPrint('More button is pressed.'))
+              ],
+              //右侧按钮
+              elevation: 30.0,
+              //appbar 阴影 默认4.0
+              bottom: TabBar(tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike))
+              ]),
+            ),
+            body: TabBarView(children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ]
+            )
+        )
     );
   }
 }

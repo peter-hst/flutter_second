1c963a39c5bc8787fa6882f3db74d98615bec336
togo.huang <huangshengtao365@zzumall.com>
2019-04-03 08:37:34 +0800

    PageView的使用, 一级PageView的属性配置

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,37 +3,45 @@ import 'package:flutter/material.dart';
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return PageViewDemo();
+  }
+}
+
+class PageViewDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
     return PageView(
-    /*  pageSnapping: false, // true:手指滑动page到屏幕一半时自动翻页,
+      /*  pageSnapping: false, // true:手指滑动page到屏幕一半时自动翻页,
       reverse: true, // 倒序排列视图*/
-    scrollDirection: Axis.vertical, //控制视图滚动的方向
-      onPageChanged: (currentPage) => debugPrint('current page at the $currentPage'),
+      scrollDirection: Axis.horizontal, //控制视图滚动的方向
+      controller: PageController(
+          initialPage: 1,
+          keepPage: true, //默认是true,这个参数表示是否记住当前页
+          viewportFraction: 0.80 // 视图占据屏幕尺寸, 默认1.0表示占据所有
+      ),
+      onPageChanged: (currentPage) =>
+          debugPrint('current page at the $currentPage'),
       children: <Widget>[
         Container(
           color: Colors.brown[900],
           alignment: Alignment(0.0, 0.0),
-          child: Text(
-            'ONE',
-            style: TextStyle(fontSize: 32.0, color: Colors.white)
-          ),
+          child: Text('ONE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
         ),
         Container(
           color: Colors.grey[900],
           alignment: Alignment(0.0, 0.0),
-          child: Text(
-              'TWO',
-              style: TextStyle(fontSize: 32.0, color: Colors.white)
-          ),
+          child: Text('TWO',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
         ),
         Container(
           color: Colors.blueGrey[900],
           alignment: Alignment(0.0, 0.0),
-          child: Text(
-              'THREE',
-              style: TextStyle(fontSize: 32.0, color: Colors.white)
-          ),
+          child: Text('THREE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
         )
       ],
     );
   }
+
 }

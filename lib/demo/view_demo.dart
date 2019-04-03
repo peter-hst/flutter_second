e264ac45410445e14ad181d5498f41c4e15dac6f
togo.huang <huangshengtao365@zzumall.com>
2019-04-03 09:40:16 +0800

    GridView使用builder

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
+
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ViewDemoCount();
+//    return GridViewCountDemo();
+    return GridViewBuilderDemo();
+  }
+}
+
+class GridViewBuilderDemo extends StatelessWidget {
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+        child: Image.network(posts[index].imageUrl, fit: BoxFit.cover));
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 150.0),
+    /*  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // 每行几列
+          crossAxisSpacing: 8.0, //交叉轴间隔
+          mainAxisSpacing: 8.0 //主轴间隔
+          ),*/
+      itemBuilder: _gridItemBuilder,
+      itemCount: posts.length,
+    );
+  }
 }
-class ViewDemoCount extends StatelessWidget {
+
+//Extend 和 Count 都是固定数量的网格,如果动态长度 请用builder
+class GridViewExtendDemo extends StatelessWidget {
   List<Widget> _builderTiles(int length) {
     return List.generate(
         length,
@@ -20,6 +45,35 @@ class ViewDemoCount extends StatelessWidget {
               ),
             ));
   }
+
+  @override
+  Widget build(BuildContext context) {
+//    return ViewDemoBuilderDemo();
+//    return GridViewCountDemo();
+    return GridView.extent(
+      maxCrossAxisExtent: 150.0,
+//      scrollDirection: Axis.horizontal,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      children: _builderTiles(100),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
+  List<Widget> _builderTiles(int length) {
+    return List.generate(
+        length,
+        (int index) => Container(
+              color: Colors.grey[300],
+              alignment: Alignment(0.0, 0.0),
+              child: Text(
+                'Item $index',
+                style: TextStyle(fontSize: 18.0, color: Colors.grey),
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
 //    return ViewDemoBuilderDemo();

78102e9f610382b80976a0f3e7d8eeeac9591147
togo.huang <huangshengtao365@zzumall.com>
2019-04-03 10:51:14 +0800

    SliverGrid设置参数 增加padding和safeArea

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -8,21 +8,33 @@ class SliverDemo extends StatelessWidget {
       //条子,口子的意思, google翻译是从一大块中切开一个口子
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 8.0,
-                  childAspectRatio: 1.0, //控制网格显示的比例
-                  mainAxisSpacing: 8.0),
-              delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) => Container(
-                        child: Image.network(posts[index].imageUrl,
-                            fit: BoxFit.cover),
-                      ),
-                  childCount: posts.length //设置列表的长度
-              ))
+          SliverSafeArea( // SlaverSafeArea 是顶部超出屏幕部分,保证内容不出屏幕,在安全区域内显示
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
+            ),
+          )
         ],
       ),
     );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            childAspectRatio: 1.0, //控制网格显示的比例
+            mainAxisSpacing: 8.0),
+        delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) => Container(
+                  child:
+                      Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+                ),
+            childCount: posts.length //设置列表的长度
+            ));
   }
 }

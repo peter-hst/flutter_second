bf682ddd52216487b657a565b3068ff5fc8057ba
togo.huang <huangshengtao365@zzumall.com>
2019-04-03 08:49:09 +0800

    PageView的Builder使用

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold
+                )
+              ),
+              Text(
+                posts[index].author
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageViewDemo();
+    return PageView.builder(
+        itemCount: posts.length, itemBuilder: _pageItemBuilder);
   }
 }
 
-class PageViewDemo extends StatelessWidget{
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
@@ -18,7 +51,7 @@ class PageViewDemo extends StatelessWidget{
           initialPage: 1,
           keepPage: true, //默认是true,这个参数表示是否记住当前页
           viewportFraction: 0.80 // 视图占据屏幕尺寸, 默认1.0表示占据所有
-      ),
+          ),
       onPageChanged: (currentPage) =>
           debugPrint('current page at the $currentPage'),
       children: <Widget>[
@@ -43,5 +76,4 @@ class PageViewDemo extends StatelessWidget{
       ],
     );
   }
-
 }
